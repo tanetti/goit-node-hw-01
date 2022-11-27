9baccf2fb0a7a653e1e5c897a8b517f6becfb6ee
Volodymyr Dudka <tanetti22@gmail.com>
2022-11-27 17:40:43 +0200

    Add commander controls

--- contacts.js
+++ contacts.js
@@ -10,7 +10,7 @@ const readContactsData = async () => {
 
     return contacts;
   } catch (error) {
-    console.error(error);
+    console.warn("\x1B[31m " + error);
   }
 };
 
@@ -20,7 +20,7 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   if (isNaN(parseInt(contactId)))
-    return console.error(`Invalid ID - ${contactId}`);
+    return console.warn(`\x1B[31m Invalid ID - ${contactId}.`);
 
   const contactsData = await readContactsData();
 
@@ -29,17 +29,53 @@ const getContactById = async (contactId) => {
   );
 
   if (!resultContact.length)
-    return console.error(`There is no contact with ID - ${contactId}.`);
+    return console.warn(`\x1B[31m There is no contact with ID - ${contactId}.`);
 
   console.table(resultContact);
 };
 
-const removeContact = (contactId) => {
-  // ...твой код
+const removeContact = async (contactId) => {
+  if (isNaN(parseInt(contactId)))
+    return console.warn(`\x1B[31m Invalid ID - ${contactId}.`);
+
+  const contactsData = await readContactsData();
+
+  const resultContacts = contactsData.filter(
+    (contact) => contact.id !== contactId.toString()
+  );
+
+  if (resultContacts.length === contactsData.length)
+    return console.warn(`\x1B[31m There is no contact with ID - ${contactId}.`);
+
+  try {
+    fs.writeFile(contactsPath, JSON.stringify(resultContacts));
+    console.log(`Contact with ID - ${contactId} was successfully deleted.`);
+  } catch (error) {
+    console.warn("\x1B[31m " + error);
+  }
 };
 
-const addContact = (name, email, phone) => {
-  // ...твой код
+const addContact = async (name, email, phone) => {
+  if (!name) return console.warn("\x1B[31m Name is required.");
+  if (!email) return console.warn("\x1B[31m Email is required.");
+  if (!phone) return console.warn("\x1B[31m Phone is required.");
+
+  const contactsData = await readContactsData();
+
+  const nextId =
+    Math.max(...contactsData.map((contact) => parseInt(contact.id))) + 1;
+
+  const resultContactsData = [
+    ...contactsData,
+    { id: nextId.toString(), name, email, phone },
+  ];
+
+  try {
+    fs.writeFile(contactsPath, JSON.stringify(resultContactsData));
+    console.log(`Contact "${name}" was successfully added.`);
+  } catch (error) {
+    console.warn("\x1B[31m " + error);
+  }
 };
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+const { Command } = require("commander");
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 } = require("./contacts");
+const program = new Command();
 
-// listContacts();
-getContactById(6);
+program
+  .option("-a, --action <type>", "Choose action")
+  .option("-i, --id <type>", "User id")
+  .option("-n, --name <type>", "User name")
+  .option("-e, --email <type>", "User email")
+  .option("-p, --phone <type>", "User phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
